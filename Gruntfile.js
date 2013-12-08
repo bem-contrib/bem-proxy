459115b229115aca7200e11184136ba2ae5f4afb
Ilya Rogov <ilyar.software@gmail.com>
2013-12-08 07:56:21 +0400

    move options jshint in native  file

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,28 +5,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     jshint: {
-      src: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          require: true,
-          define: true,
-          requirejs: true,
-          describe: true,
-          expect: true,
-          it: true
-        }
-      }
+      src: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
     },
 
     mocha: {
