c8f4684b835ed982539bc1ed7f2e94ab9295dbfe
Alexey Yaroshevich <zxqfox@gmail.com>
2017-01-28 06:02:36 +0300

    rework

--- app.js
+++ app.js
@@ -1,47 +1,46 @@
+const fs = require('fs');
+const http = require('http');
 
-var http = require('http'),
-  bemRender = require('lib/bem-render-middleware'),
-  httpProxy = require('http-proxy');
+const App = require('./');
 
-httpProxy.createServer(
-    bemRender.pages('pages'),
-    function (req, res, next) {
-        res.setHeader('X-Powered-By', 'bem-proxy');
-        next();
-    },
-    9000, 'localhost'
-).listen(8001);
+const app = App({
+    target: 'http://127.0.0.1:8087',
+    listen: '3333'
+});
 
-http.createServer(function (req, res) {
-    require('fs').readFile('bem-examples/www/pages/example/example.bemjson.js', function (err, data) {
-        if (err) {
-            res.writeHead(500);
-            res.end('Something went wrong: ' + e.message);
-            res.end();
-            return;
-        }
-        res.writeHead(200, { 'Content-Type': 'application/x-bemjson' });
-        res.write(data);
-        res.end();
+app.on('listen', address => {
+    console.log('Proxy server listening on ' + (address.port ? 'port ' + address.port : address));
+    address.port || process.on('exit', function(e) {
+        fs.unlinkSync(address);
     });
-}).listen(9000);
+});
+process.on('SIGINT', () => process.exit());
 
-/*
-http.createServer(function (req, res) {
-    http.get("http://interactive-answers.yandex.ru/api/2.x/site/d89cda99-274e-47be-abe5-ac0ed339a083/search", function (gres) {
-        res.writeHead(200, { 'Content-Type': 'application/x-bemjson' });
-        res.write('request successfully proxied: ' + req.url +'\n');
-        res.write(JSON.stringify(req.headers, true, 2));
-        res.write(JSON.stringify(gres.headers, true, 2));
-        gres.on('data', function (chunk) {
-            res.write(chunk);
-        });
-        gres.on('end', function (chunk) {
-            res.end();
-        });
-    }).on('error', function (e) {
-        res.writeHead(500);
-        res.end('Something went wrong: ' + e.message);
-    });
-}).listen(9000);
-*/
+
+
+// -----------------------------------------------------------------------------
+// Apache emulation below:
+
+const server2 = http.createServer(function(req, res) {
+    switch (req.url) {
+        case '/':
+        case '/index.html':
+            res.writeHead('200', { 'Content-Type': 'text/html' });
+            res.write(`<html><body><h1>It's a title</h1><p>Pure HTML`);
+            break;
+        case '/bem.html':
+            res.writeHead('200', { 'Content-Type': 'text/html', 'X-Content-Type': 'application/x-bemjson' });
+            res.write(JSON.stringify([
+                '<html><body>',
+                { tag: 'h1', content: `It's a title` },
+                { tag: 'p', content: 'BEMJSON' }
+            ]));
+            break;
+        default:
+            res.writeHead('404', { 'Content-Type': 'text/html' });
+            res.write(`<html><body><h1>Not found</h1><p>Supported pages: /index.html, /bem.bemjson.js, /bem.html`);
+    }
+    res.end();
+});
+
+server2.listen(8087, () => console.log('Listening php-emulation server on ' + server2.address().port));
